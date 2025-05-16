4922afc14dd4fc3348e66950e4e8eb36c0062c1c
Mauri03C <rgmauricio86@gmail.com>
2025-05-16 11:16:42 -0500

    Formulario y estilos agregados

--- my-proyect/src/pages/BlogPage.jsx
+++ my-proyect/src/pages/BlogPage.jsx
@@ -1,12 +1,46 @@
-import HeaderComponent from "../components/HeaderComponent"
+import HeaderComponent from "../components/HeaderComponent";
 
 function BlogPage() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("El formulario se ha enviado");
+  };
+
   return (
     <>
-    <HeaderComponent></HeaderComponent>
-    <div>BlogPage</div>
+      <HeaderComponent />
+
+      <div className="blog-container">
+        <h1 className="form-title">Tu opinión nos importa</h1>
+
+        <form className="opinion-form" onSubmit={handleSubmit}>
+          <fieldset>
+            <label htmlFor="username">Nombre:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Escribe tu nombre"
+              required
+            />
+          </fieldset>
+
+          <fieldset>
+            <label htmlFor="comentario">Comentario:</label>
+            <textarea
+              id="comentario"
+              name="comentario"
+              placeholder="Escribe tu comentario"
+              rows="4"
+              required
+            ></textarea>
+          </fieldset>
+
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default BlogPage
+export default BlogPage;
--- my-proyect/src/pages/HomePage.jsx
+++ my-proyect/src/pages/HomePage.jsx
@@ -1,13 +1,18 @@
 import HeaderComponent from "../components/HeaderComponent"
 
-
 function HomePage() {
+  const saySomething = () => {
+    console.log("Di Algo");
+    alert("Ahora");
+  }
+
   return (
     <>
       <HeaderComponent></HeaderComponent>
       <div>HomePage</div>
+      <hr></hr>
+      <button onClick={saySomething}>Pulsa aquí</button>
     </>
-    
   )
 }
 
